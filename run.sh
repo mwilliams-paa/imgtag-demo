3a910f4151007ce76ced56dfceefb2ba3e5a395f
Marshall Williams <mwilliams@paanalytics.com>
2020-05-01 11:49:56 -0500

    added dev "branch" tag

--- run.sh
+++ run.sh
@@ -3,17 +3,20 @@ BUILD_NUMBER="$RANDOM"
 echo "Creating entropy file"
 echo ${BUILD_NUMBER} > entropy
 
+CURRENT_BRANCH="dev"
 IMAGE_BUILD_REPO="quay.io/mwpaa/tagdemo"
 IMAGE_BUILD_TAG="${IMAGE_BUILD_REPO}:${BUILD_NUMBER}"
 
 # Build the image
 echo "Building ${IMAGE_BUILD_TAG}"
-docker build -q -t ${IMAGE_BUILD_TAG} .
+docker build --no-cache -q -t ${IMAGE_BUILD_TAG} .
 echo
 
 # Push the image
 echo "Pushing ${IMAGE_BUILD_TAG}"
 docker push ${IMAGE_BUILD_TAG}
+docker tag ${IMAGE_BUILD_TAG} "${IMAGE_BUILD_REPO}:${CURRENT_BRANCH}"
+docker push "${IMAGE_BUILD_REPO}:${CURRENT_BRANCH}"
 echo
 
 read -p "Promote to prod? (y/n) " choice
